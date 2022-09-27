03d64b70e1859832b8400798071db98ef73ec8fa
Matias-Galvan <matiasgalvan90@gmail.com>
2022-09-26 23:03:40 -0300

    gente cagona del age

--- tp2/Conjunto.cs
+++ tp2/Conjunto.cs
@@ -1,11 +1,79 @@
-// namespace tp1.tp2
-// {
-//     public class Conjunto : IColeccionable
-//     {
-//         private List<IComparable> listaConjunto;
+namespace tp1.tp2
+{
+    public class Conjunto : IColeccionable
+    {
+        private List<IComparable> listaConjunto;
 
-//         public Conjunto(){
-//             listaConjunto = new List<IComparable>();
-//         }
-//     }
-// }
+        public Conjunto(){
+            listaConjunto = new List<IComparable>();
+        }
+        
+        public bool pertenece(IComparable comparable){
+            bool aux = false;
+            for (int i = 0; i < listaConjunto.Count; i++)
+            {
+                if (listaConjunto[i].sosIgual(comparable))
+                {
+                    aux= true;
+                    break;
+                }
+            }
+            return aux;
+        }
+        public void agregar(IComparable comparable){
+            if (pertenece(comparable))
+            {
+                for (int i = 0; i < listaConjunto.Count; i++)
+                {
+                    if (listaConjunto[i].sosIgual(comparable))
+                    {
+                        listaConjunto[i] = comparable;
+                        break;
+                    }
+                }
+            }else
+            {
+                listaConjunto.Add(comparable);
+            }
+        }
+
+        public int cuantos(){
+            return listaConjunto.Count;
+        }
+
+        public IComparable minimo(){
+            IComparable aux = listaConjunto[0];
+
+            for (int i = 0; i < listaConjunto.Count; i++)
+            {
+                if (listaConjunto[i].esMenor(aux))
+                {
+                    aux = listaConjunto[i]; 
+                }
+            }
+            return aux;
+        }
+
+        public IComparable maximo(){
+            IComparable aux = listaConjunto[0];
+
+            for (int i = 0; i < listaConjunto.Count; i++)
+            {
+                if (listaConjunto[i].esMayor(aux))
+                {
+                    aux = listaConjunto[i];
+                }
+            }
+            return aux;
+        }
+
+        public bool contiene(IComparable comparable){
+            bool aux = false;
+
+            for (int i = 0; i < listaConjunto.Count; i++)
+            {
+                
+            }
+        }
+    }
+}
